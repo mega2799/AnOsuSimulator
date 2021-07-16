cafc717dc2ddbf10979f679de1330f81b9fdef85
mega2799 <megadigimatt@gmail.com>
2021-07-16 11:40:45 +0200

    Now the view uses the controller to play audio

--- src/it/unibo/osu/View/DisplayMenu.java
+++ src/it/unibo/osu/View/DisplayMenu.java
@@ -1,28 +1,43 @@
 package it.unibo.osu.View;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import it.unibo.osu.Controller.MusicControllerImpl;
 import it.unibo.osu.Player.MusicPlayer;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class DisplayMenu {
 	
 	private static final double SCENE_WIDTH = 1280;
 	private static final double SCENE_HEIGHT = 720;
-	private Pane gameRoot = new Pane();
+	//private Pane gameRoot = new Pane();
+	private StackPane gameRoot = new StackPane();
 	private Scene gameScene;
-	private static MusicPlayer mp;
+	private static MusicControllerImpl audio;
+	
+	
+	private ImageView im;
+	
 	
 	private GameMenu gameOptions;
 
 	public DisplayMenu(final Stage primary) {
+		im = new ImageView();
+		
+		im.setImage(new Image(new File("/res/gif/intro.gif").toURI().toString()));
+		//im.setImage(new Image(this.getClass().getResource("intro.gif").toExternalForm()));
+		//questa cosa dovrebbe funzionare.... non sei riscito a mettere res come risorsa effettiva...
+		
 		primary.setTitle("USo!");
 		
 		gameOptions = new GameMenu(SCENE_WIDTH, SCENE_HEIGHT);
@@ -35,12 +50,17 @@ public class DisplayMenu {
 
 		this.gameScene = new Scene(this.gameRoot, SCENE_WIDTH, SCENE_HEIGHT);
 		
-		mp = new MusicPlayer("res/tracks/joshiraku.wav");
+		//mp = new MusicPlayer("joshiraku.wav");
+		audio = new MusicControllerImpl("/tracks/joshiraku.wav");
 		
-		this.mp.run();
+		audio.startMusic();
 
-		primary.setScene(gameScene);
+	//	mp = new MusicPlayer("res/tracks/joshiraku.wav");
 		
+		//this.mp.run();
+		
+		primary.setScene(gameScene);
+	
 		primary.show();
 	
 }
@@ -48,7 +68,7 @@ public class DisplayMenu {
 	private void setImage(GameMenu g) throws IOException {
 		InputStream is = Files.newInputStream(Paths.get("res/wallpaper/uso.png"));
 		Image background = new Image(is);
-		is.close();
+		is.close(); // chiudo lo stream
 		ImageView imgview = new ImageView(background);
 		imgview.setFitWidth(SCENE_WIDTH);
 		imgview.setFitHeight(SCENE_HEIGHT);
