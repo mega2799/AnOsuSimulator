8da10c722cb0237d4b6366df624303aa57eb9b5a
ManuelLuzietti <manuel.luzietti@studio.unibo.it>
2021-08-10 01:28:58 +0200

    mapped hitcircles in a smaller area.

--- src/it/unibo/osu/util/Mapper.java
+++ src/it/unibo/osu/util/Mapper.java
@@ -3,16 +3,19 @@ package it.unibo.osu.util;
 import it.unibo.osu.model.Hitpoint;
 
 public class Mapper {
-	public final static double WIDTH = 1920;
-	public final static double HEIGHT = 1080;
+	public final static double WIDTH = 1840;
+	public final static double XOFFSET = 80;
+	public final static double HEIGHT = 910;
+	public final static double YOFFSET = 234;
+	
 	public final static double BEATMAPWIDTH = 640;
 	public final static double BEATMAPHEIGHT = 480;
 	
 	public static Hitpoint mapHitpoint(Hitpoint hitpoint) {
 		double x = hitpoint.getX();
 		double y = hitpoint.getY();
-		double newX = (x / Mapper.BEATMAPWIDTH) * Mapper.WIDTH;
-		double newY = (y / Mapper.BEATMAPHEIGHT) * Mapper.HEIGHT;
+		double newX = (x  * (WIDTH - XOFFSET) / BEATMAPWIDTH) + XOFFSET;
+		double newY = (y  * (HEIGHT - YOFFSET) / BEATMAPHEIGHT) + YOFFSET;
 		return new Hitpoint(newX, newY, hitpoint.getTime());
 	}
 }
