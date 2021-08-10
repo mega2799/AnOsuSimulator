8cc8c7795dd17a94adfcd762895c3fe448d6545e
mega2799 <megadigimatt@gmail.com>
2021-08-10 09:59:28 +0200

    utilities for menu

---
+++ src/it/unibo/osu/View/NeonButton.java
@@ -0,0 +1,36 @@
+package it.unibo.osu.View;
+
+import java.io.IOException;
+
+import com.sun.prism.paint.Color;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+
+public class NeonButton {
+	private Button button;
+
+	public Button getButton() {
+		return this.button;
+	}
+
+	public NeonButton(final String text) {
+		try {
+			this.button = FXMLLoader.load(this.getClass().getResource("/view/NeonPinkButton.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		this.button.setText(text);
+
+		this.button.setOnMouseEntered(e -> {
+			this.button.setStyle("-fx-background-color: pink; -fx-background-radius: 50;");
+		});
+
+		this.button.setOnMouseExited(e -> {
+			this.button.setStyle("-fx-background-color: white; -fx-background-radius: 50;");
+		});
+
+	}
+
+}
