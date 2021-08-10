1e44fb2f86806f23cc6f6d98b6ea7c95f70f544d
mega2799 <megadigimatt@gmail.com>
2021-08-10 09:56:48 +0200

    add a skeleton for end game menu

--- src/it/unibo/osu/View/EndGameView.java
+++ src/it/unibo/osu/View/EndGameView.java
@@ -2,16 +2,52 @@ package it.unibo.osu.View;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class EndGameView  extends Stage{
+	private BorderPane root;
+	private GridPane statPane;
+	
 	public EndGameView() {
-		Pane root = new Pane();
+		this.root = new BorderPane();
+		this.statPane = new GridPane();
+		
+		gridSetter();
 
 		this.setScene(new Scene(root,600,600));
-		root.getChildren().add(new Label("EndGame"));
+		
+		this.root.setTop(new Label("qua ci va il nome della canzone"));
+		this.root.setCenter(statPane);
+		this.root.setBottom(new Label("NOME GIOCATORE + PUNTI DELLA BETAMAP"));
+		//root.getChildren().add(new Label("EndGame"));
 		this.show();
+	}
+
+	private void gridSetter() {
+		this.statPane.setGridLinesVisible(true);
+		
+		this.statPane.getColumnConstraints().add(new ColumnConstraints(200));
+		this.statPane.getColumnConstraints().add(new ColumnConstraints(200));
+		
+
+		this.statPane.getRowConstraints().add(new RowConstraints(100));
+		this.statPane.getRowConstraints().add(new RowConstraints(100));
+		this.statPane.getRowConstraints().add(new RowConstraints(100));
+
+		this.statPane.add(new Label("OK:"), 0, 0);
+		this.statPane.add(new Label("100"), 1, 0);
+	
+		this.statPane.add(new Label("GREAT:"), 0, 1);
+		this.statPane.add(new Label("200"), 1, 1);
+
+	
+		this.statPane.add(new Label("MEH:"), 0, 2);
+		this.statPane.add(new Label("300"), 1, 2);
 	}
 
 }
